2adc66186414e865bdc8befe70818749e3db2b0b
sianlouisa <sianlynch6@gmail.com>
2021-11-19 14:22:08 +0000

    initial set up

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hey :) I'm a Software Engineer from Manchester and I love Next.js!
+        </p>
+      </section>
+    </Layout>
+  );
+}
